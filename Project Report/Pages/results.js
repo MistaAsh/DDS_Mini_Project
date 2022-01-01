c2d98aa04eea9eea33c5742f47d27aaf13a0b064
Asim Jawahir <asimjawahir123@gmail.com>
2022-01-02 00:24:58 +0530

    Toggalable button

---
+++ Project Report/Pages/results.js
@@ -0,0 +1,33 @@
+function change() // no ';' here
+{
+    var btn = document.getElementById("mybtn");
+    btn.value = 'my value'; // will just add a hidden value
+    // btn.innerHTML = '1';
+    if (btn.innerHTML == '0') {
+        btn.innerHTML = '1';
+    } else {
+        btn.innerHTML = '0';        
+    }
+}
+function change1() // no ';' here
+{
+    var btn = document.getElementById("mybtn1");
+    btn.value = 'my value'; // will just add a hidden value
+    // btn.innerHTML = '1';
+    if (btn.innerHTML == '0') {
+        btn.innerHTML = '1';
+    } else {
+        btn.innerHTML = '0';        
+    }
+}
+function change2() // no ';' here
+{
+    var btn = document.getElementById("mybtn2");
+    btn.value = 'my value'; // will just add a hidden value
+    // btn.innerHTML = '1';
+    if (btn.innerHTML == '0') {
+        btn.innerHTML = '1';
+    } else {
+        btn.innerHTML = '0';        
+    }
+}
